9adb7599cf20d79b3ce4b6f68263aeb74d366552
Erik Safaryan <ero.safaryan.2002@gmail.com>
2023-05-12 17:25:44 +0400

    Added property

--- BookStore/Book.cs
+++ BookStore/Book.cs
@@ -8,5 +8,58 @@ namespace BookStore
 {
     public class Book
     {
+        bool IsValueChanged = false;
+
+        public bool isvaluechanged
+        {
+            get { return isvaluechanged; }  
+            set { isvaluechanged = value; }
+        }
+
+        public int ID { get; set; }
+
+        public string? BookName;
+
+        public string bookname
+        {
+            get { return BookName; }
+            set
+            {
+                if (BookName != value)
+                {
+                    IsValueChanged = true;
+                    bookname = value;
+                }
+            }
+        }
+
+        public int? PublicationYear
+        {
+            get { return PublicationYear; }
+
+            set
+            {
+                if(PublicationYear != value)
+                {
+                    IsValueChanged = true;
+                    PublicationYear = value;
+                }
+            }
+        }
+
+        public int? Autor
+        {
+            get { return PublicationYear; }
+
+            set
+            {
+                if (PublicationYear != value)
+                {
+                    IsValueChanged = true;
+                    PublicationYear = value;
+                }
+            }
+        }
+
     }
 }
